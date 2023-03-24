f12fa030c225d622aa5360dc6a763bbda7e3b5f2
Pittawat Taveekitworachai <11158905+Pittawat2542@users.noreply.github.com>
2023-03-24 09:48:42 +0900

    init: initialize project

---
+++ README.md
@@ -0,0 +1,18 @@
+# Qualification Checking Script
+
+This repository contains a script for checking qualification of each prompt file.
+
+## Installation
+
+To use this script, you must have <a href="https://nodejs.org/en/" target="_new">Node.js</a> and <a href="https://www.npmjs.com/" target="_new">npm</a> installed on your system.
+
+1. Clone this repository to your local machine.
+2. Navigate to the repository directory in your terminal.
+3. Run `npm install` to install the necessary dependencies.
+
+## Usage
+
+1. Run the script using the command `npm start -s="<SOURCE_FOLDER>"`. For example, `npm start -s="./competition"`.
+2. The script will output the copied of original file if passed the qualification in `../<SOURCE_FOLDER>/qualified` folder, which has the same structure as the source folder. The file `qualification_log_<DATE_TIME>.txt` will be created in the `../<SOURCE_FOLDER>/qualified/logs` folder.
+
+Please ensure that the source folder contains only text files.
---
+++ src/index.ts
@@ -0,0 +1,60 @@
+import { appendLog, createLogFolder, createOutputFolder, listAllDirs, listAllFiles, listCharactersDirs } from './file-utils';
+import { containDisallowedCharacters, containObjectTokens, countWords } from 'chatgpt4pcg';
+
+import fs from 'fs'
+import parseArgs from 'minimist'
+import path from 'path'
+
+const MAX_WORDS = 900
+
+async function main() {
+  const args = parseArgs(process.argv.slice(2))
+  const argv = process.platform === 'win32' ? args['_'] : args['s']
+  if (argv === undefined) {
+    throw Error('Insufficient parameters to work with.')
+  }
+
+  const sourceFolder = argv + '/'
+  const sFolder = path.posix.resolve(sourceFolder)
+  
+  const outputDir = path.posix.join(sFolder, '..', 'qualified')
+  if (!fs.existsSync(outputDir)) {
+    await fs.promises.mkdir(outputDir)
+  }
+
+  const logFolderPath = await createLogFolder(outputDir)
+
+  const promptsFiles = await listAllFiles(sFolder)
+  for (const promptFile of promptsFiles) {
+    const promptFilePath = path.posix.join(sFolder, promptFile)
+    const fileContent = await fs.promises.readFile(promptFilePath, 'utf-8')
+
+    const hasExceedMaxLength = countWords(fileContent) > MAX_WORDS
+    const exceedLengthLog = `[${new Date().toISOString().replaceAll(':', '_')}] Processing - team: ${promptFile.split('.').slice(0, -1).join('.')} - maxLength: ${hasExceedMaxLength ? 'Failed' : 'Success'}`
+    await appendLog(logFolderPath, exceedLengthLog)
+
+    if (hasExceedMaxLength) {
+      continue
+    }
+
+    const hasContainDisallowChars = containDisallowedCharacters(fileContent)
+    const disallowCharLog = `[${new Date().toISOString().replaceAll(':', '_')}] Processing - team: ${promptFile.split('.').slice(0, -1).join('.')} - disallowChars: ${hasContainDisallowChars ? 'Failed' : 'Success'}`
+    await appendLog(logFolderPath, disallowCharLog)
+
+    if (hasContainDisallowChars) {
+      continue
+    }
+
+
+    const hasNoObjectToken = !containObjectTokens(fileContent)
+    const objectTokenLog = `[${new Date().toISOString().replaceAll(':', '_')}] Processing - team: ${promptFile.split('.').slice(0, -1).join('.')} - objectToken: ${hasNoObjectToken ? 'Failed' : 'Success'}`
+    await appendLog(logFolderPath, objectTokenLog)
+
+    if (hasNoObjectToken) {
+      continue
+    }
+
+    await fs.promises.copyFile(promptFilePath, path.posix.join(outputDir, promptFile))
+  }
+}
+main()
